97a48252deb410da98c09512205b2a3651776d45
eliconstantopoulos <kakoueee@gmail.com>
2024-04-14 19:29:22 +0100

    win+loss components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { IntroComponent } from './intro/intro.component';
+import { WinComponent } from './win/win.component';
+import { LossComponent } from './loss/loss.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
-    IntroComponent
+    IntroComponent,
+    WinComponent,
+    LossComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/loss/loss.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loss',
+  templateUrl: './loss.component.html',
+  styleUrls: ['./loss.component.scss']
+})
+export class LossComponent {
+
+}
